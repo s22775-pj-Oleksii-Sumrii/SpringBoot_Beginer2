aa5965680a34bb8cf641cb57cd6bc645dc89d70e
s22775-pj-Oleksii-Sumrii <s22775@pjwstk.edu.pl>
2024-04-19 17:58:21 +0200

    Mapping

--- src/main/java/org/example/program/MapStructCar.java
+++ src/main/java/org/example/program/MapStructCar.java
@@ -2,10 +2,12 @@ package org.example.program;
 
 import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.ERROR, componentModel = "spring", builder = @Builder(disableBuilder = true))
 public interface MapStructCar {
+    @Mapping(target = "id", ignore = true)
     Car toEntity(CarCreateRequest request);
     CarResponse mapToCarResponse(Car car);
 }
