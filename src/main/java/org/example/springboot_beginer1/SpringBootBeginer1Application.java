041b01ff324eb5753b72a1ea509b5aa7a5282dba
s22775-pj-Oleksii-Sumrii <s22775@pjwstk.edu.pl>
2024-05-28 22:36:40 +0200

    Swagger(?)

--- src/main/java/org/example/springboot_beginer1/SpringBootBeginer1Application.java
+++ src/main/java/org/example/springboot_beginer1/SpringBootBeginer1Application.java
@@ -2,11 +2,43 @@ package org.example.springboot_beginer1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
+//@EnableSwagger2
 public class SpringBootBeginer1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootBeginer1Application.class, args);
-    }
+        SpringApplication.run(SpringBootBeginer1Application.class, args);}
+
+//    @Bean
+//    public Docket get() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .paths(PathSelectors.ant("/api/**"))
+//                .apis(RequestHandlerSelectors.basePackage("pl.sumrii.springbootswaggerexample"))
+//                .build().apiInfo(createApiInfo());
+//    }
+//
+//    private ApiInfo createApiInfo() {
+//        return new ApiInfo("Cars API",
+//                "Cars database",
+//                "1.00",
+//                "http://sumrii.pl",
+//                new Contact("Alex", "http://sumrii.pl", "alex.sumriy@sumrii.pl"),
+//                "my own licence",
+//                "http://sumrii.pl",
+//                Collections.emptyList()
+//        );
+//    }
 }
